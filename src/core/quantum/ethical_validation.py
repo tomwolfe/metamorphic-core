d8407a5c44a73d186c5e40442c69d27cc593c050
Thomas Wolfe <tomwolfe@gmail.com>
2025-01-21 20:39:24 -0600

    Update ethical_validation.py

--- src/core/quantum/ethical_validation.py
+++ src/core/quantum/ethical_validation.py
@@ -1,4 +1,5 @@
-from qiskit import QuantumCircuit, Aer, execute
+from qiskit import QuantumCircuit, execute
+from qiskit.providers.aer import Aer  # Explicit import for Aer
 import numpy as np
 
 class EthicalQuantumCore:

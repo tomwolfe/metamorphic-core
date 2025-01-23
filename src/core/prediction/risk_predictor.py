0e06b67bf639fb5243a284dfee24c0b4d6c8efb4
Thomas Wolfe <tomwolfe@gmail.com>
2025-01-22 18:14:12 -0600

    Update risk_predictor.py

--- src/core/prediction/risk_predictor.py
+++ src/core/prediction/risk_predictor.py
@@ -5,17 +5,17 @@ from qiskit_aer import Aer
 import numpy as np
 
 class QuantumRiskPredictor:
-    """Quantum risk predictor with proper parameter handling"""
+    """Quantum risk predictor with proper variational form integration"""
     
     def __init__(self, num_qubits=3, time_steps=5):
         self.num_qubits = num_qubits
         self.time_steps = time_steps
         self.backend = Aer.get_backend('aer_simulator')
-        self.params = ParameterVector('θ', length=num_qubits*2)
+        self.params = ParameterVector('θ', length=num_qubits*4)  # Increased parameter space
         self._build_circuit()
 
     def _build_circuit(self):
-        """Build parameterized circuit with valid Qiskit parameters"""
+        """Build parameterized circuit with valid parameter binding"""
         self.circuit = QuantumCircuit(self.num_qubits)
         
         # Parameterized rotation layer
@@ -26,14 +26,17 @@ class QuantumRiskPredictor:
         for i in range(self.num_qubits-1):
             self.circuit.cx(i, i+1)
             
-        # Variational form
+        # Variational form with direct parameter mapping
         var_form = RealAmplitudes(self.num_qubits, reps=1)
-        for param in var_form.parameters:
-            self.circuit.append(
-                var_form.to_instruction([param]),
-                range(self.num_qubits)
-            )
-            
+        param_mapping = {
+            var_param: self.params[i % len(self.params)]
+            for i, var_param in enumerate(var_form.parameters)
+        }
+        self.circuit.compose(
+            var_form.assign_parameters(param_mapping),
+            inplace=True
+        )
+        
         self.circuit.measure_all()
 
     def forecast_risk(self, history: list) -> dict:
@@ -45,15 +48,16 @@ class QuantumRiskPredictor:
         }
         transpiled = transpile(
             self.circuit.assign_parameters(param_bind),
-            self.backend
+            self.backend,
+            optimization_level=1
         )
         job = self.backend.run(transpiled, shots=1000)
         counts = job.result().get_counts()
         
         return {
-            'immediate_risk': counts.get('0'*self.num_qubits, 0)/1000,
-            'midterm_risk': np.mean(list(counts.values()))/1000,
-            'longterm_risk': max(counts.values())/1000
+            'immediate': counts.get('0'*self.num_qubits, 0)/1000,
+            'short_term': np.mean(list(counts.values()))/1000,
+            'long_term': max(counts.values())/1000
         }
 
     def _preprocess(self, data: list) -> np.ndarray:

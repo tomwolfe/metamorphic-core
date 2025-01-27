2b854c14fee96798a618446d41d298dfbd04605b
Thomas Wolfe <tomwolfe@gmail.com>
2025-01-27 01:34:45 -0600

    Update __init__.py

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,6 +1,8 @@
 # Add new exports
 from .quantum.state_preservation import QuantumStatePreserver
 from .ethics.governance import EthicalAuditLogger
+from .ethics.governance import QuantumEthicalValidator
+from .quantum.ethical_validation import EthicalQuantumCore
 
 __all__ = [
     'QuantumEthicalValidator',

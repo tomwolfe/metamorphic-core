47324635af11606488b46d4a0b1b93a47cd5b4d7
Thomas Wolfe <tomwolfe@gmail.com>
2025-02-10 18:22:08 -0600

    Update test_agent_pipeline.py

--- tests/integration/test_agent_pipeline.py
+++ tests/integration/test_agent_pipeline.py
@@ -20,7 +20,7 @@ def validator():
 
     with patch('src.core.agents.security_agent.SecurityAgent.run_zap_baseline_scan') as mock_zap, \
          patch('src.core.agents.test_generator.TestGenAgent.generate_tests') as mock_tests, \
-         patch('subprocess.run') as mock_subprocess_run,  \ # Mock subprocess.run - comma moved to this line
+         patch('subprocess.run') as mock_subprocess_run,  \
          patch.dict(os.environ, valid_mocks),
          patch('src.utils.config.SecureConfig.get', lambda *args, **kwargs: valid_mocks.get(args[0]) if args else None):
 

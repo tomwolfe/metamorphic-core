d1f06c3fbf3cc26f85e330d21769b8e455091bd8
Thomas Wolfe <tomwolfe@gmail.com>
2025-02-11 22:46:47 -0600

    Revert "Update __init__.py"
    This reverts commit 1ff1f28516bfb29e22c0fa01940578ef3360c963.

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -3,11 +3,6 @@ from .quantum.state_preservation import QuantumStatePreserver
 from .ethics.governance import EthicalAuditLogger
 from .ethics.governance import QuantumEthicalValidator
 from .quantum.ethical_validation import EthicalQuantumCore
-import os
-import sys
-
-# Ensure core modules are importable
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 __all__ = [
     'QuantumEthicalValidator',

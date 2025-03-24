f52a1c39a0e0da0ba3080b7c9fe4e3164681940a
Thomas Wolfe <tomwolfe@gmail.com>
2025-03-23 21:16:57 -0500

    updated

--- src/api/server.py
+++ src/api/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
